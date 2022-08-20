934264187e2a7baacfe1f577d0d1579db2af7761
Marnel Gnacadja <gmlginolias@gmail.com>
2022-08-20 21:58:36 +0100

    working on answer vote

---
+++ src/Controller/AnswerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Answer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnswerController extends AbstractController
+{
+
+    #[Route('/answers/{id}/vote', name: 'app_answer_vote')]
+    public function vote(Answer $answer, Request $request) {
+
+    }
+
+}
